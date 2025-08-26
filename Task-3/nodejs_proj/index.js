d62bf88a1ee90c5e0b4df69b71ea5101df9b6f4f
hemanithin22 <hemanithins@celcomsolutions.com>
2025-08-26 11:37:00 +0530

    port change

--- Task-3/nodejs_proj/index.js
+++ Task-3/nodejs_proj/index.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const path = require('path');
 
 const hostname = '127.0.0.1';
-const port = 3000;
+const port = 3001;
 
 const server = http.createServer((req, res) => {
   if (req.url === '/style.css') {
